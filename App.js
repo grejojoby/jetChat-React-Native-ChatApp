ddddd988ea9e31d70272bc48a50c125e4a2cf225
Grejo Joby <grejo00@gmail.com>
2021-09-19 16:22:45 +0530

    sceens

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Screen name='Login' component={LoginScreen} />
+      
+    </NavigationContainer>
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCp4Z-eqgVyA7Q_f1_STo3QWlvtr9zYSy4",
+    authDomain: "jotok-2ba19.firebaseapp.com",
+    projectId: "jotok-2ba19",
+    storageBucket: "jotok-2ba19.appspot.com",
+    messagingSenderId: "326369861277",
+    appId: "1:326369861277:web:48b638c2cd925260973e74"
+  };
