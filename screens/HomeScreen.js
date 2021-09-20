428a1a1fe708feea5285f2055f68054c870ff2cc
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-09-20 13:45:42 +0530

    updated the nav bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,6 +4,7 @@ import {  ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-nat
 import { Avatar } from 'react-native-elements';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { auth,db } from '../firebase.js';
+import {AntDesign , SimpleLineIcons} from "@expo/vector-icons"
 
 import CustomListItem from '../components/CustomListItem.js';
 const HomeScreen = ({navigation}) => {
@@ -19,17 +20,30 @@ const HomeScreen = ({navigation}) => {
             headerTitleStyle:{color:"black"},
             headerTintColor:"black",
             headerTitleAlign: 'center',
-            headerLeft:()=><View style={{marginLeft:0}}>
+            headerLeft:()=>(<View style={{marginLeft:0}}>
                 <TouchableOpacity activeOpacity={0.5} onPress={signOutUser}>
                 <Avatar
                 rounded
-                source={{uri:auth?.currentUser?.photoURL}}
+                source={{uri:auth?.currentUser?.photoURL?auth?.currentUser?.photoURL:"https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.png"}}
                 />
                 </TouchableOpacity>
                 
-            </View>
+            </View>),
+            headerRight:()=>(<View style={{
+                flexDirection:"row",
+                justifyContent:"space-between",
+                width:60,
+                marginRight:5
+            }}>
+                <TouchableOpacity activeOpacity={0.5}>
+                <AntDesign name="camerao" size={24} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity activeOpacity={0.5}>
+                <SimpleLineIcons name="pencil" size={24} color="black" />
+                </TouchableOpacity>
+            </View>)
         });
-    },[])
+    },[navigation])
     return (
         <SafeAreaView>
             <ScrollView>
