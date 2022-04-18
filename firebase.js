98d820c3ee10c49426dee53c35fa0e0171b79ab5
Grejo Joby <grejo00@gmail.com>
2022-04-18 21:00:43 +0530

    firebase file

--- firebase.js
+++ firebase.js
@@ -1,7 +1,9 @@
-import * as firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC42m_4cJ3j6C5mtZHzctqoxRb63I5uEhc",
   authDomain: "rnchat-d0d3f.firebaseapp.com",
@@ -9,14 +11,15 @@ const firebaseConfig = {
   storageBucket: "rnchat-d0d3f.appspot.com",
   messagingSenderId: "546537876108",
   appId: "1:546537876108:web:e69c8ba9301856bcff29a9"
-  };
+};
 
-let app;
-if (firebase.apps.length==0){
-   app = firebase.initializeApp(firebaseConfig);
-} else {
-  app=firebase.app()
-}
+// Initialize Firebase
+// let app;
+// if (firebase.apps.length==0){
+app = initializeApp(firebaseConfig);
+// } else {
+  // app=firebase.app()
+// }
 
 const db= app.firestore();
 const auth=firebase.auth();
